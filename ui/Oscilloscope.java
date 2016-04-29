645a0ef8864c757e7bc9f5df8a87a9c14963a506
Ishola Babatunde Isaac <isholabi@gmail.com>
2016-04-29 10:25:02 -0500

    Update Oscilloscope.java

--- ui/Oscilloscope.java
+++ ui/Oscilloscope.java
@@ -11,6 +11,7 @@ import java.util.List;
 /**
  *
  * @author  Adedayo Omitayo adedayo.omitayo@gmail.com
+ * modified by: Ishola Babatunde
  */
 public class Oscilloscope extends javax.swing.JPanel {
 
